4f737c8fc1c3f3e6f4458ce25042db3fb1249148
atulaksingh <talktoatulsingh@gmail.com>
2025-09-03 14:31:35 +0530

    about and product done

--- src/landing_page/about/AboutPage.js
+++ src/landing_page/about/AboutPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './Hero'
+import Team from './Team'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+   <>
+     <Hero />
+      <Team />
+   </>
   )
 }
 
